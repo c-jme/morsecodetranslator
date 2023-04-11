786ca247e39c7ec6ede5a13a43b846c6664d71f1
c-jme <110544806+c-jme@users.noreply.github.com>
2023-04-11 13:48:07 +1000

    final changes

--- JS/morscode.test.js
+++ JS/morscode.test.js
@@ -10,9 +10,44 @@ import { engChar, toMorse, morseChar, toEng } from "./translator.js";
 
 
 describe('Checks for text to morse', () => {
-  it('Returns expected english-to-morse translations.', () => {
+  it('Returns expected english-to-morse translations, including spaces, certain special characters, and numbers.', () => {
     expect(toMorse("Bananas", text)).toBe(
       "-... .- -. .- -. .- ..."
     );
+    expect(toMorse("This is a space", text)).toBe(
+      "- .... .. ... / .. ... / .- / ... .--. .- -.-. ."
+    );
+    expect(toMorse("555", text)).toBe(
+      "..... ..... ....."
+    );
+  });
+
+  it('Returns error when invalid characters are entered in english input field', () => {
+    expect(engChar('<', text)).toBe(
+      "Please enter in valid English characters that do not include the following: (~,`, <, >, {, }, |, *, ^, %)"
+    );
+    expect(engChar('b%a%n%anas', text)).toBe(
+      "Please enter in valid English characters that do not include the following: (~,`, <, >, {, }, |, *, ^, %)"
+    );
   })
-})
+});
+
+
+
+describe('Checks for morse to text', () => {
+  it('Returns expected result from morse-to-english', () => {
+    expect(toEng('-.-- . ...', text)).toBe(
+      "yes"
+    );
+    expect(toEng('.... --- .-.. -.-- / -... .- -. .- -. .- ...', text)).toBe(
+      "holy bananas"
+    );
+    expect(toEng('.---- ..--- ...-- ....-')).toBe(
+      "1234"
+    );
+  });
+
+  it('Returns error when invalid characters are entered in the morse input field', () => {
+    expect(morseChar())
+  })
+});
